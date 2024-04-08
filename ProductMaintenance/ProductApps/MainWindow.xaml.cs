e48b4fe31790a87d4b334ccd32d85f725b61fb07
GEONHWI SONG <geonhwi.song@student.tafesa.edu.au>
2024-04-08 12:54:51 +0930

    “Commit the branch feat_wrap1.1

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25);
+                totalChargewithWrap.Text = Convert.ToString(cProduct.TotalPayment + 25 + 5);
             }
             catch (FormatException)
             {
